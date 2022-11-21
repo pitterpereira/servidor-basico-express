da3580ef446c37ab54b62ce2bc210bce1fcbbaa1
Pitter Pereira <pitterpereira@gmail.com>
2022-11-21 15:37:31 -0300

    Update and rename calculadora.js to index.js

--- index.js
+++ index.js
@@ -9,17 +9,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-// Post para a própria página
-app.post("/", (req, res) => {
-    var num = Number(req.body.num1) + Number(req.body.num2);
-    res.send("O resultado é: " + num);
-});
-
-app.post("/subtracao", (req, res) => {
-    var num = Number(req.body.num1) - Number(req.body.num2);
-    res.send("O resultado é: " + num);
-});
-
 app.listen(3000, () => {
     console.log('Servidor rodando!');
 });
