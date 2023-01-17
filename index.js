cbdeda0dc0ffb2634afd6c3d89ccd968a2cd38e1
Pitter Pereira da Costa <pitterpereira@gmail.com>
2023-01-16 21:28:20 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+// Requisita o módulo express, insalado via npm
 const express = require('express');
 
 const app = express();
 
+// Middleware - seleciona a biblioteca para o parsing de conteúdo
 app.use(express.urlencoded({extended: true}));
 
-// respond with "hello world" when a GET request is made to the homepage
+// Quando uma solicitação GET é feita para a home, responde enviando o arquivo index.html
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
     console.log('Servidor rodando!');
 });
